b11999f7e8564ccdbd793879976f1d9a4d69a613
Charles Khoza <khozacharles832@gmail.com>
2026-09-13 12:52:20 +0200

    deployment config setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import path from 'path';
+import { clerkMiddleware } from '@clerk/express';
 import { ENV } from './config/env.js';
+import { connectDB } from './config/db.js';
 
 const app = express();
 
 const __dirname = path.resolve();
+
+app.use(clerkMiddleware()); // register Clerk middleware for authentication
 
 app.get("/api/health", (req, res) => {
     res.status(200).json({ message: "Server is healthy" });
@@ -17,7 +21,9 @@ if(ENV.NODE_ENV === 'production') {
 
 
 
-app.listen(ENV.PORT, () => console.log(`Server is running on port ${ENV.PORT}`));
+app.listen(ENV.PORT, () => {
+    connectDB(); // Connect to the database when the server starts
+    console.log(`Server is running on port ${ENV.PORT}`)});
 
 app.get("/{*any}", (req, res) => {
     res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
